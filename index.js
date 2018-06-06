209e0690c03cc10ab1eb843ca9f25a80af1a308c
Andy Puertas <andy.puertas08@gmail.com>
2018-06-06 14:36:02 -0600

    on step 5

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const Auth0Strategy = require('passport-auth0');
+const passport = require('passport');
 
 const app = express();
+
+app.use(session({
+    secret: 'lolmao',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+passport.use( new Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/login',
+    scope: 'open id profile'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    done(null, profile)
+}))
+
+
 
 
 const port = 3000;
